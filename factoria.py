cdb7fb0d01a1ab680f56e9c02405082c538ecd15
VENUJAN <VENUJAN@users.noreply.github.com>
2017-07-25 11:50:48 +0530

    Create factoria.py

---
+++ factoria.py
@@ -0,0 +1,10 @@
+v = int(input("enter the number"))
+fact = 1
+if(fact < 0):
+print(" negative number not using fo calculate for factorial")
+elif(fact = 0)
+print("factorial of 0 is 1")
+else:
+for s in range (v,v+1)
+fact = fact * s
+print("the fatorial of " ,v, "is",fact )
