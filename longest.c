e05fd4bc544540c72c909c08db8f189afe591767
VENUJAN <VENUJAN@users.noreply.github.com>
2017-07-25 11:07:09 +0530

    Create longest.c

---
+++ longest.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+  int n,i;
+  char v[50],s[50],e[50],b[50];
+  gets(v);
+  gets(s);
+  gets(b);
+  n=strlen(a);
+  for(i=0;i<n;i++)
+  {
+      if(v[i]==s[i])
+      {
+        b[i]=v[i];
+      }else
+      {
+          break;
+      }
+  }for(i=0;i<n;i++)
+  {
+      if(s[i]==b[i])
+      {
+          printf("%c",c[i]);
+      }else
+      {
+          break;
+      }
+  }
+    
+}
+
